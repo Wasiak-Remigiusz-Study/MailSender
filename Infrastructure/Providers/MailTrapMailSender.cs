dd21e99ccbc7e3a89b8b1644e842bcee80f513db
Samekmat <samekmat@gmail.com>
2026-06-19 17:05:39 +0200

    fix(mailtrap): revert sender domain to demomailtrap.co

--- Infrastructure/Providers/MailTrapMailSender.cs
+++ Infrastructure/Providers/MailTrapMailSender.cs
@@ -23,7 +23,7 @@ public class MailTrapMailSender : IMailSenderProvider
         {
             from = new
             {
-                email = "hello@demomailtrap.com",
+                email = "hello@demomailtrap.co",
                 name = "Mailtrap Test"
             },
             to = new[]
