8bebfa63d4e0721db5be1dddf3d2667aaa946be3
Samekmat <samekmat@gmail.com>
2026-06-19 19:11:06 +0200

    fix(webclient): read token from input field instead of env variable

--- WebClient/main.ts
+++ WebClient/main.ts
@@ -5,6 +5,9 @@ OpenAPI.BASE = "http://localhost:5110";
 OpenAPI.TOKEN = import.meta.env.VITE_API_TOKEN;
 
 async function sendMail() {
+  const token = (document.getElementById("token") as HTMLInputElement).value;
+  OpenAPI.TOKEN = token;
+
   const to = (document.getElementById("to") as HTMLInputElement).value;
   const subject = (document.getElementById("subject") as HTMLInputElement)
     .value;
